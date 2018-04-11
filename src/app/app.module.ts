2c372b1df5f681bce01d94ef15239ede8b30874f
vinhdv <duongvinh.itdv3@gmail.com>
2018-04-11 11:17:18 +0700

    masster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule, AngularFireDatabase } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+import { DatabaseServiceProvider } from '../providers/database-service/database-service';
+export const config = {
+   apiKey: "AIzaSyC7e6iSaPczCufJXKqNsfW5nj7FbHjAvuY" ,
+   authDomain: "fir-frirebase.firebaseapp.com" ,
+   databaseURL: "https://fir-frirebase.firebaseio.com" ,
+   projectId: "fir-frirebase" ,
+   storageBucket: "fir-frirebase.appspot.com" ,
+   messagingSenderId: "855378024205"
+};
+@NgModule ({
+   declarations: [
+      MyApp ,
+      HomePage ,
+      ListPage
+   ] ,
+   imports: [
+      BrowserModule ,
+      IonicModule.forRoot (MyApp) ,
+      AngularFireModule.initializeApp (config) ,
+      AngularFireDatabaseModule ,
+      AngularFireAuthModule
+   ] ,
+   bootstrap: [IonicApp] ,
+   entryComponents: [
+      MyApp ,
+      HomePage ,
+      ListPage
+   ] ,
+   providers: [
+      StatusBar ,
+      SplashScreen ,
+      AngularFireDatabase,
+      {provide: ErrorHandler , useClass: IonicErrorHandler} ,
+      DatabaseServiceProvider
+   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+import { DatabaseServiceProvider } from "../../providers/database-service/database-service";
+@Component ({
+   selector: 'page-home' ,
+   templateUrl: 'home.html'
 })
 export class HomePage {
+   playlistItems:Array<any> = [];
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+   constructor (public navCtrl:NavController , private fbDbProvider:DatabaseServiceProvider) {
+      const data = this.fbDbProvider.getData ().valueChanges ().toPromise();
+      console.log (data);
+   }
 }
